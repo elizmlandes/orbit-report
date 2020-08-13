df443f18ed9a1c7057dd93f961d1410a879c9ba7
Elizabeth Landes <elizmlandes@gmail.com>
2020-08-12 21:04:28 -0500

    final commit

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -13,6 +13,7 @@ sourceList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,9 +24,23 @@ sourceList: Satellite[];
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
           }
-
+          this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
 
   }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+ }
 }
